08115249811ecc01c827979bfa4e2437c1dced04
oswealth <osbertjaywealth@gmail.com>
2024-06-25 04:25:12 +0000

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,20 @@
+'use strict';
 import $ from 'jquery';
-import { debounce } from 'lodash';
+import _ from 'lodash';
 
-$(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="start-button">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-  let count = 0;
+const updateCounter = () => {
+  let counts = $('#count').html() || 0;
+  $('button').on('click', () => {
+    counts++;
+    $('#count').html(`${counts} clicks on the button`);
+  });
+};
 
-  function updateCounter() {
-    count++;
-    $('#count').html(`${count} clicks on the button`);
-  }
-
-  $('#start-button').on('click', debounce(updateCounter, 300));
-});
+_.debounce(updateCounter, 500);
+updateCounter();
