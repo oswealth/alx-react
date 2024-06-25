9c979795f3795f4430277988514ceb140722e2bd
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-06-25 05:13:59 +0000

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,21 @@
+'use strict';
+import '../css/main.css';
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+  let counts = $('#count').html() || 0;
+  $('button').on('click', () => {
+    counts++;
+    $('#count').html(`${counts} clicks on the button`);
+  });
+};
+
+_.debounce(updateCounter, 500);
+updateCounter();
