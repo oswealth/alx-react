5684f37ae401dfefca9e0e980e13a35e42613755
oswealth <osbertjaywealth@gmail.com>
2024-06-25 06:00:15 +0000

    Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,7 @@
+
+import './footer.css';
+import $ from 'jquery';
+
+$(function() {
+	$('body').append('<p>Copyright - Holberton School</p>');
+});
---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,9 @@
+
+import './header.css';
+import $ from 'jquery';
+
+$(function() {
+	$('body').append("<div id='logo'></div>");
+	$('body').append('<h1>Holberton Dashboard</h1>');
+	console.log('Init header');
+});
