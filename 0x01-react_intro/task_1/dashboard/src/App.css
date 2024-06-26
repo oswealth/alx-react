5b67cc1de4bd236fdaef59a6595938d48b82f7f6
oswealth <osbertjaywealth@gmail.com>
2024-06-26 03:40:26 +0000

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/App.css
@@ -0,0 +1,37 @@
+.App {
+	height: 100vh;
+	max-width: 100vw;
+	--App-border-bottom: 4px solid red;
+	--App-padding: 1.2em;
+	--header-font-size: 1.4rem;
+	--App-font-size: 1.4rem;
+	font-family: "Arial", Helvetica, sans-serif;
+}
+
+.App-header {
+	font-size: var(--header-font-size);
+	color: red;
+	display: flex;
+	align-items: center;
+	padding: var(--App-padding);
+	border-bottom: var(--App-border-bottom);
+}
+
+.App-header img {
+	width: 250px;
+	height: 250px;
+}
+
+.App-body {
+	font-size: var(--App-font-size);
+	padding: var(--App-padding);
+	border-bottom: var(--App-border-bottom);
+	height: 45%;
+}
+
+.App-footer {
+	font-size: var(--App-font-size);
+	padding: var(--App-padding);
+	text-align: center;
+	font-style: italic;
+}
