aefe4fdd463b3acfa35c9fb83c61b74eca91061e
oswealth <osbertjaywealth@gmail.com>
2024-06-27 15:33:20 -0700

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,11 +1,11 @@
-import logo from './Holberton-logo.jpg';
+import logo from './holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <div className='App-header'>
-        <img src={logo} alt='Holberton' />
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="Holberton-logo" />
         <h1>School dashboard</h1>
       </div>
       <div className='App-body'>
