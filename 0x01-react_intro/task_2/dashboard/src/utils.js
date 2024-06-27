66731584e815b86b40a3beb4b743d4043a27dff1
oswealth <osbertjaywealth@gmail.com>
2024-06-27 15:36:03 -0700

    Modify the App and Notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,7 +1,11 @@
-export const getFullYear = () => new Date().getFullYear();
+export function getFullYear() {
+    return new Date().getFullYear();
+}
 
-export const getFooterCopy = (isIndex) => 
-    isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+export function getFooterCopy(isIndex) {
+    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
 
-export const getLatestNotification = () =>
-    '<strong>Urgent requirement</strong> - complete by EOD';
+export function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - Complete by EOD';
+}
