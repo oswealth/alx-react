ed1f3ead5383b52ceae619bb87fb7234f274bf96
oswealth <osbertjaywealth@gmail.com>
2024-06-27 15:38:42 -0700

    Create react tests, basic tests with four tests and Install Enzyme

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,31 +1,25 @@
+import logo from './holberton-logo.jpg';
 import './App.css';
-import logo from './Holberton-logo.jpg';
-import { getFullYear, getFooterCopy } from './utils';
+import { getFooterCopy, getFullYear } from './utils';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-header">
+        <img src={logo} alt="Holberton-logo" />
         <h1>School dashboard</h1>
-      </header>
-      <hr />
-      <body className="App-body">
+      </div>
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
-        <label>Email:
-          <input type="text" name="email"/>
-        </label>
-        <label>Password:
-          <input type="password" name="password"/>
-        </label>
+        <label htmlFor='email'>Email:</label>
+        <input type="email" id="email" name="email" autoComplete='email' />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" autoComplete='off' />
         <button>OK</button>
-      </body>
-      <hr id="footer-margin"/>
-      <footer className="App-footer">
-        <p>
-          <em>Copyright {getFullYear()} - {getFooterCopy(false)}</em>
-        </p>
-      </footer>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
     </div>
   );
 }
--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,14 +1,22 @@
-import { getFullYear, getFooterCopy, getLatestNotification } from './utils'
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-test ('returns the correct year', () => {
-    expect(getFullYear()).toBe(new Date().getFullYear());
+describe('Utils functions', () => {
+    test('getFullYear returns the correct year', () => {
+        const currentYear = new Date().getFullYear();
+        expect(getFullYear()).toBe(currentYear);
+    });
+
+    test('getFooterCopy returns correct string when isIndex is true', () => {
+        expect(getFooterCopy(true)).toBe('Holberton School');
+    });
+
+    test('getFooterCopy returns correct string when isIndex is false', () => {
+        expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+    });
+
+    test('getLatestNotification return correct string', () => {
+        const expectedString = '<strong>Urgent requirement</strong> - complete by EOD';
+        expect(getLatestNotification()).toBe(expectedString);
+    });
 });
 
-test ('returns the correct string', () => {
-    expect(getFooterCopy(true)).toBe('Holberton School');
-    expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-});
-
-test ('returns current string', () => {
-    expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
-});
