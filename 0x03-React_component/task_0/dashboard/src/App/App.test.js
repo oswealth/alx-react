7e5d4f6191ab264cd233fa4d033620ddb7439507
oswealth <osbertjaywealth@gmail.com>
2024-07-03 22:14:24 +0000

    Commence with class components

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+
+describe('App component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('contains the Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications)).toHaveLength(1);
+  });
+
+  it('contains the Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).toHaveLength(1);
+  });
+
+  it('contains the Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login)).toHaveLength(0);
+  });
+
+  it('contains the Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer)).toHaveLength(1);
+  });
+
+  it("does not render CourseList if not logged in", () => {
+    const wrapper = shallow(<App isLoggedIn={false} />);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+    expect(wrapper.find(Login)).toHaveLength(1);
+  });
+
+  it("renders CourseList if logged in", () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.find(CourseList)).toHaveLength(1);
+    expect(wrapper.find(Login)).toHaveLength(0);
+  });
+});
---
+++ 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CourseShape from './CourseShape';
+import CourseListRow from './CourseListRow';
+import './CourseList.css';
+
+function CourseList({ listCourses = [] }) {
+    return (
+        <table id="CourseList">
+            <thead>
+                <CourseListRow textFirstCell="Available courses" isHeader={true} />
+                <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+            </thead>
+            <tbody>
+                {listCourses.length > 0 ? (
+                    listCourses.map(({ id, name, credit }) => (
+                        <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />
+                    ))
+                ) : (
+                    <CourseListRow textFirstCell="No course available yet" />
+                )}
+            </tbody>
+        </table>
+    );
+}
+
+CourseList.propTypes = {
+    listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultProps = {
+    listCourses: [],
+};
+
+export default CourseList;
+
